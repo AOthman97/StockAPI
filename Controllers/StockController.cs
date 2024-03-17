16d1552f13ea66d3f2851da90c6b8b68a09235a8
aho <ahmad.othman@intalio.com>
2024-03-18 01:23:49 +0300

    Basic edits for the stock controller

--- Controllers/StockController.cs
+++ Controllers/StockController.cs
@@ -1,15 +1,43 @@
 ﻿using api.Data;
+using api.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace api.Controllers
 {
     [Route("api/stock")]
     [ApiController]
-    // This is the new primary constructor way with directly putting our param in the class definition
     public class StockController(ApplicationDBContext context) : ControllerBase
     {
         private readonly ApplicationDBContext _context = context;
+
+        // Advanced feature, New null checking without actually checking for nulls each time in code
+        //public class NullStock : Stock
+        //{
+        //    public NullStock() 
+        //    {
+        //        return;
+        //    }
+        //}
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var stocks = _context.Stocks.ToList();
+
+            return Ok(stocks);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetById([FromRoute] int id) 
+        {
+            var stock = _context.Stocks.Find(id);
+
+            if(stock == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(stock);
+        }
     }
-
-
 }
