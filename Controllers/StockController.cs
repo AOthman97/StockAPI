57bb6c93028f0f03691c9cc0e9517d23e71b20a0
aho <ahmad.othman@intalio.com>
2024-03-18 00:43:28 +0300

    Add project files.

---
+++ Controllers/StockController.cs
@@ -0,0 +1,15 @@
+﻿using api.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace api.Controllers
+{
+    [Route("api/stock")]
+    [ApiController]
+    // This is the new primary constructor way with directly putting our param in the class definition
+    public class StockController(ApplicationDBContext context) : ControllerBase
+    {
+        private readonly ApplicationDBContext _context = context;
+    }
+
+
+}
