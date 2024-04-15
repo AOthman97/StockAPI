0604043a173d5a01ec978ee3f0eb44fec62447ea
Ahmad OTHMAN <ahmad.othman@intalio.com>
2024-04-15 09:31:18 +0300

    Added the register endpoint using the User Manager after seeding the DB with initial roles

---
+++ Migrations/20240414083517_SeedRoles.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace api.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeedRoles : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[,]
+                {
+                    { "6a75b1bc-9714-409f-951e-62a5abc7e9f4", null, "Admin", "ADMIN" },
+                    { "dd4145b3-9e6d-4bb9-9db8-29e21b107ca5", null, "User", "USER" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "6a75b1bc-9714-409f-951e-62a5abc7e9f4");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "dd4145b3-9e6d-4bb9-9db8-29e21b107ca5");
+        }
+    }
+}
